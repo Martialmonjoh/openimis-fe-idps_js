948910639e7c92e64704b4044e6ad769a4cc689f
vaneck237 <vaneckbilongo@gmail.com>
2022-12-06 18:22:39 +0100

    probleme avec le dropdown de l'année

--- src/pickers/PerformanceYearPicker.js
+++ src/pickers/PerformanceYearPicker.js
@@ -1,15 +1,38 @@
-import React, { Component , useState } from "react";
+import React, { Component, useState } from "react";
 import { injectIntl } from "react-intl";
 import _ from "lodash";
-import { SelectInput } from "@openimis/fe-core";
+import { SelectInput, formatMessage } from "@openimis/fe-core";
 
-//const setSelectedYear = useState(null);
+const INIT_STATE = {
+  value: null,
+  label: null
+};
 
 class PerformanceYearPicker extends Component {
+  state = INIT_STATE;
 
   render() {
-    const { intl, module , label = "year", ...others } = this.props;
-    const options = [];
+    const {
+      intl,
+      name,
+      value,
+      module,
+      label,
+      nullLabel = "year.null",
+      min,
+      max,
+      onChange,
+      withNull = true,
+    } = this.props;
+    
+    const options = withNull
+      ? [
+        {
+          value: null,
+          label: formatMessage(intl, module, nullLabel),
+        },
+      ]
+      : [];
 
     options.push(
       ..._.range(2000, new Date().getFullYear() + 1).map((v) => ({
@@ -18,13 +41,15 @@ class PerformanceYearPicker extends Component {
       }))
     );
 
-    _onChange = (v) => {
-      this.setState({ value: v }, (e) => {
-        this.props.onChange(v, setSelectedYear(v));
-      });
-    };
-
-    return <SelectInput module={module} label={label} options={options} onChange={this._onChange} {...others}/>;
+    return (
+      <SelectInput
+        module={module}
+        label={label}
+        options={options}
+        name={name}
+        value={value}
+        onChange={onChange} />
+    );
   }
 }
 
