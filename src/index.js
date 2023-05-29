d2b246af63bed8941b53837747d0f5c4198fafcc
devo21 <mboulefacklekaneloic@gmail.com>
2023-05-29 17:37:39 +0100

    #25803 affichage des rapport des idps

--- src/index.js
+++ src/index.js
@@ -3,26 +3,35 @@ import messages_fr from "./translations/fr.json";
 import IdpsMainMenu from './menu/IdpsMainMenu';
 import PerfomancePage from "./pages/PerfomancePage";
 import PerformancesPage from "./pages/PerformancesPage";
+import ReportsPage from "./pages/ReportsPage";
 import PerformanceMonthPicker from "./pickers/PerformanceMonthPicker";
 import PerformanceYearPicker from "./pickers/PerformanceMonthPicker";
+import EnrolledFamiliesReport from "./reports/EnrolledFamiliesReport";
+import InsureeFamilyOverviewReport from "./reports/InsureeFamilyOverviewReport";
 import reducer from "./reducer"
 
 const ROUTE_IDPS_PERFORMANCE = "idps/performance";
 const ROUTE_IDPS_PERFORMANCES = "idps/performances";
+const ROUTE_IDPS_REPORTS = "idps/reports";
 
 const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }, { key: "fr", messages: messages_fr }],
   "reducers": [{key: 'idps', reducer}],
+  "reports": [
+
+  ],
   "refs": [
     { key: "idps.PerformanceMonthPicker", ref: PerformanceMonthPicker },
     { key: "idps.PerformanceYearPicker", ref: PerformanceYearPicker },
 
     { key: "idps.route.performance", ref: ROUTE_IDPS_PERFORMANCE },
-    { key: "idps.route.performances", ref: ROUTE_IDPS_PERFORMANCES }
+    { key: "idps.route.performances", ref: ROUTE_IDPS_PERFORMANCES },
+    { key: "idps.route.reports", ref: ROUTE_IDPS_REPORTS },
   ],
   "core.Router": [
     { path: "idps/performance", component: PerfomancePage },
-    { path: "idps/performances", component: PerformancesPage }
+    { path: "idps/performances", component: PerformancesPage },
+    { path: "idps/reports", component: ReportsPage },
   ],
   "core.MainMenu": [IdpsMainMenu],
 }
